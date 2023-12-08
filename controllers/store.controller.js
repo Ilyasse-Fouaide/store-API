252369a177f6554e345f4ea26fa900213ea99153
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-08 16:17:40 +0100

    Wrap try-catch around the response

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -1,3 +1,13 @@
-module.exports.index = (req, res) => {
-  res.status(200).json({ message: "Welcome to the API" });
+module.exports.index = async (req, res) => {
+  try {
+    res.status(200).json({ message: "Welcome to the API" });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: {
+        status: 500,
+        message: "Internal Server Error"
+      }
+    })
+  }
 }
