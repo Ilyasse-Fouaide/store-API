b55d94ca687456ddb1e3536d1fcc9c78d5c4cf13
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-08 15:50:32 +0100

    Revert  to the codebase

--- main.js
+++ main.js
@@ -6,25 +6,9 @@ dotenv.config();
 
 const app = express();
 
-const promise = () => {
-  return new Promise((resolve, reject) => {
-    reject();
-  });
-}
-
-app.get("/ok", async (req, res, next) => {
-  try {
-    await promise()
-    res.status(200).json({ message: result });
-  } catch (error) {
-    next("app broke")
-  }
-});
-
-app.use((err, req, res, next) => {
-  console.log("logged...");
-  res.status(500).json({ error: err })
-});
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "ok!." });
+})
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Listening to the port ${port}...`));
