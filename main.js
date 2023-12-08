1eeb6b8139f98fa6cf33c14b32511a40e23a4e15
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-08 15:43:08 +0100

    Just Writing a simple error handlers in express.

--- main.js
+++ main.js
@@ -6,8 +6,24 @@ dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "ok!." });
+const promise = () => {
+  return new Promise((resolve, reject) => {
+    reject();
+  });
+}
+
+app.get("/ok", async (req, res, next) => {
+  try {
+    await promise()
+    res.status(200).json({ message: result });
+  } catch (error) {
+    next("app broke")
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log("logged...");
+  res.status(500).json({ error: err })
 });
 
 const port = process.env.PORT || 5001;
