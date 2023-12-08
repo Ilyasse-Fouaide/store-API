39a8095911f389804475097edf9d1fb1f8903481
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-08 15:10:59 +0100

    Added a route to the root URL that returns a JSON response with a message of "ok!."

--- main.js
+++ main.js
@@ -6,5 +6,9 @@ dotenv.config();
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "ok!." });
+});
+
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Listening to the port ${port}...`));
