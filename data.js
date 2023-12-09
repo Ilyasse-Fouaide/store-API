a5dcbc5e1ff4c53827ce2165be240ac64c30f830
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-09 07:53:55 +0100

    add data for products

---
+++ data.js
@@ -0,0 +1,67 @@
+const data = [
+  {
+    "name": "Miller",
+    "price": 46902,
+    "feature": true,
+    "rating": 4,
+    "company": "Jasper"
+  },
+  {
+    "name": "Flores",
+    "price": 40576,
+    "feature": true,
+    "rating": 4,
+    "company": "Earbang"
+  },
+  {
+    "name": "Marylou",
+    "price": 10448,
+    "feature": true,
+    "rating": 5,
+    "company": "Lingoage"
+  },
+  {
+    "name": "Carolina",
+    "price": 12858,
+    "feature": true,
+    "rating": 4,
+    "company": "Namebox"
+  },
+  {
+    "name": "Solis",
+    "price": 38342,
+    "feature": true,
+    "rating": 5,
+    "company": "Zilodyne"
+  },
+  {
+    "name": "Charles",
+    "price": 31665,
+    "feature": true,
+    "rating": 5,
+    "company": "Portico"
+  },
+  {
+    "name": "Sheena",
+    "price": 11287,
+    "feature": true,
+    "rating": 4,
+    "company": "Nutralab"
+  },
+  {
+    "name": "Silvia",
+    "price": 40222,
+    "feature": false,
+    "rating": 4,
+    "company": "Hinway"
+  },
+  {
+    "name": "Bond",
+    "price": 2766,
+    "feature": false,
+    "rating": 4,
+    "company": "Zomboid"
+  }
+];
+
+module.exports = data;
