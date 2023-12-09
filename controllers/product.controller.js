04e3e31dd2cffc445a9a406fa1b870077208bd10
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-09 08:42:49 +0100

    Update Product model to use `limit` instead of `strict: false`

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,7 +5,7 @@ const data = require("../data");
 module.exports.index = tryCatchWrapper(async (req, res) => {
   const { features, limit } = req.query;
 
-  const products = await Product.find({ features }, undefined, { strict: false, limit, });
+  const products = await Product.find({ features }, undefined, { limit, });
 
   res.status(200).json({
     success: true,
