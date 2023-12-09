7ba4f3f3313e9d13d4259f536e7c7525b941611c
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-09 08:34:34 +0100

    Add features and limit query parameters to product search

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,7 +3,10 @@ const Product = require("../models/product.model");
 const data = require("../data");
 
 module.exports.index = tryCatchWrapper(async (req, res) => {
-  const products = await Product.find();
+  const { features, limit } = req.query;
+
+  const products = await Product.find({ features }, undefined, { strict: false, limit, });
+
   res.status(200).json({
     success: true,
     pagination: {
