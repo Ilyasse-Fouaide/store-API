5ba0c332ce8a4f849a4cc11b9b68f9da767b23a0
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-09 08:57:30 +0100

    refactoring my code

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,7 +5,9 @@ const data = require("../data");
 module.exports.index = tryCatchWrapper(async (req, res) => {
   const { features, limit } = req.query;
 
-  const products = await Product.find({ features }, undefined, { limit, });
+  const products = await Product.find({
+    features
+  }, undefined, { limit, });
 
   res.status(200).json({
     success: true,
